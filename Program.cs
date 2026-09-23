a99a697d829549233dc48c7b2adebd104c417f65
joshcoralvz <joshuacornelioalvarez@gmail.com>
2026-09-22 23:04:14 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//programa básico de consola
+Console.Write("INGRESA LOS DATOS REQUERIDOS");
+
+Console.WriteLine(" ");
+//Solicitud de datos al usuario
+//entrada de datos & almacenamiento
+Console.Write("Nombre:");
+string nombre = Console.ReadLine();
+
+Console.Write("Apellido:");
+string apellido = Console.ReadLine();
+
+Console.Write("Edad:");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Altura:");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Hola, mi nombre es {nombre} {apellido}, tengo {edad} años de edad y mido {altura} metros de altura.");
+//Salida de datos
